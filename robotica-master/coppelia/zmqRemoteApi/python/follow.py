5a9db8e4e4e7f471755da0fec01cc2104f5f00af
Daniel B <a01657103@tec.mx>
2023-06-03 12:27:34 +0200

    Update follow.py

--- robotica-master/coppelia/zmqRemoteApi/python/follow.py
+++ robotica-master/coppelia/zmqRemoteApi/python/follow.py
@@ -20,82 +20,19 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-import numpy as np
 import cv2
 import robotica
 
 
-def detect_red_ball(img):
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-    # Define the color range for red in HSV
-    lower_red = np.array([0, 70, 50])
-    upper_red = np.array([10, 255, 255])
-
-    mask = cv2.inRange(hsv, lower_red, upper_red)
-    mask = cv2.dilate(mask, None, iterations=2)
-
-    contours, _ = cv2.findContours(
-        mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    center = None
-    if len(contours) > 0:
-        # Find the largest contour
-        c = max(contours, key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        if M["m00"] > 0:
-            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-        else:
-            center = None
-
-    return center
-
-
-# Define PID controller
-class PIDController:
-    def __init__(self, Kp, Ki, Kd, dt):
-        self.Kp = Kp
-        self.Ki = Ki
-        self.Kd = Kd
-        self.dt = dt
-        self.error_sum = 0
-        self.prev_error = 0
-
-    def compute(self, error):
-        self.error_sum += error
-        error_diff = error - self.prev_error
-        control = self.Kp*error + self.Ki*self.error_sum * \
-            self.dt + self.Kd*error_diff/self.dt
-        self.prev_error = error
-        return control
-
-
 def main(args=None):
-    # Initialize CoppeliaSim connection
     coppelia = robotica.Coppelia()
     robot = robotica.P3DX(coppelia.sim, 'PioneerP3DX', True)
-    pid = PIDController(0.1, 0.01, 0.5, 0.1)  # Tune these values as needed
-
     coppelia.start_simulation()
-
     while coppelia.is_running():
         img = robot.get_image()
-        # Assume that the ball is the largest red object in the image
-        ball_position = detect_red_ball(img)
-        # The error is the horizontal distance of the ball from the center of the image
-        error = img.shape[1]/2 - ball_position[0]
-        speed = pid.compute(error)
-        # Adjust the speed of the robot based on the PID controller
-        robot.set_speed(speed, speed)
-
-        # If there's an obstacle, stop and rotate the robot
-        if robot.ultrasonic_sensors_detect_obstacle():
-            robot.set_speed(0, 0)
-            robot.rotate(90)
-
         cv2.imshow('opencv', img)
         cv2.waitKey(1)
-
+        robot.set_speed(0, 0)
     coppelia.stop_simulation()
     cv2.destroyAllWindows()
 
